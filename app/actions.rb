914ece11d6503c1f1a525c9b6b992f5b2326d6b2
Slav Kurilyak <kurilyak@gmail.com>
2014-05-29 04:39:15 +0000

    Implement authentication views and routes

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,16 @@
 get '/' do
   erb :index
 end
+
+# Authentication Routes
+get '/login' do
+  erb :'auth/login'
+end
+
+get '/signup' do
+  erb :'auth/signup'
+end
+
+get '/logout' do
+  erb :'/'
+end
