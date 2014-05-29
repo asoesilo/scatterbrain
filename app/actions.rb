6a66f0f02b2139e726a2f3e5f5da84c9f4d6179a
Slav Kurilyak <kurilyak@gmail.com>
2014-05-29 23:42:59 +0000

    Implement login, logout functionality

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,9 @@
+helpers do
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+end
+
 # Homepage (Root path)
 get '/' do
   erb :index
@@ -13,5 +19,34 @@ get '/signup' do
 end
 
 get '/logout' do
-  erb :'/'
+  session[:user_id] = nil
+  redirect '/'
+end
+
+post '/signup' do
+  @user = User.new(
+    username: params[:username],
+    password: params[:password]
+  )
+
+  if @user.save
+    session[:user_id] = @user.id
+    redirect '/'
+  else
+    erb :'auth/signup'
+  end
+end
+
+post '/login' do
+  @user = User.find_by(
+    username: params[:username], 
+    password: params[:password]
+  )
+
+  if @user
+    session[:user_id] = @user.id
+    redirect '/'
+  else
+    erb :'auth/login'
+  end
 end
