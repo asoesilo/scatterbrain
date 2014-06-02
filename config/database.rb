bfc060c783f1dc9b285fcd50a5b0f2dd5816d013
Slav Kurilyak <kurilyak@gmail.com>
2014-06-02 07:00:04 +0000

    Change to local database

--- config/database.rb
+++ config/database.rb
@@ -4,19 +4,19 @@ configure do
     ActiveRecord::Base.logger = Logger.new(STDOUT)
   end
 
-  # set :database, {
-  #   adapter: "postgresql",
-  #   database: "scatterbrain"
-  # }
-
   set :database, {
     adapter: "postgresql",
-    host: "ec2-174-129-197-200.compute-1.amazonaws.com",
-    port: "5432",
-    username: "yuetsvswtniawy",
-    password: "_N3cGPvlI2fSpscuKkPUUTp-hB",
-    database: "d7avgj2qgk4o64"
-  }  
+    database: "scatterbrain"
+  }
+
+  # set :database, {
+  #   adapter: "postgresql",
+  #   host: "ec2-174-129-197-200.compute-1.amazonaws.com",
+  #   port: "5432",
+  #   username: "yuetsvswtniawy",
+  #   password: "_N3cGPvlI2fSpscuKkPUUTp-hB",
+  #   database: "d7avgj2qgk4o64"
+  # }  
 
   # Load all models from app/models, using autoload instead of require
   # See http://www.rubyinside.com/ruby-techniques-revealed-autoload-1652.html
